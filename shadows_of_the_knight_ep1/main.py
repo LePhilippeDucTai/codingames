212d879180dbe5fc7f5c08f03a5cafe38c07df51
PhilippeL <le.philippe.duc.tai@gmail.com>
2022-07-15 23:49:05 +0200

    Simplified and cleaned the class.

--- shadows_of_the_knight_ep1/main.py
+++ shadows_of_the_knight_ep1/main.py
@@ -1,8 +1,3 @@
-import math
-
-import numpy as np
-
-
 def read_initial_config():
     with open("input.txt") as f:
         w, h = [int(x) for x in f.readline().split()]
@@ -11,48 +6,41 @@ def read_initial_config():
         return w, h, n, x0, y0
 
 
-def add(x, x0, lim):
-    return min(max(0, x + x0), lim)
+class Batman:
+    def __init__(self, x0, y0, w, h):
+        self.x0 = x0
+        self.y0 = y0
+        self.x_min = 0
+        self.x_max = w
+        self.y_min = 0
+        self.y_max = h
 
+    def update_boundaries(self, command):
+        if "U" in command:
+            self.y_max = self.y0 - 1
+        if "D" in command:
+            self.y_min = self.y0 + 1
+        if "L" in command:
+            self.x_max = self.x0 - 1
+        if "R" in command:
+            self.x_min = self.x0 + 1
 
-def reposition(dir, x0, y0, w, h):
-    if dir == "UR":
-        pos_x = (w + x0) // 2
-        pos_y = add(y0, -1, h)
-    elif dir == "DR":
-        pos_x = (w + x0) // 2
-        pos_y = add(y0, 1, h)
-    elif dir == "UL":
-        pos_x = x0 // 2
-        pos_y = add(y0, -1, h)
-    elif dir == "DL":
-        pos_x = x0 // 2
-        pos_y = add(y0, 1, h)
-    elif dir == "U":
-        pos_x = x0
-        pos_y = y0 // 2
-    elif dir == "D":
-        pos_x = x0
-        pos_y = (h + y0) // 2
-    elif dir == "L":
-        pos_x = x0 // 2
-        pos_y = y0
-    else:
-        pos_x = (w + x0) // 2
-        pos_y = y0
-    return pos_x, pos_y
+    def move(self):
+        self.x0 = (self.x_min + self.x_max) // 2
+        self.y0 = (self.y_min + self.y_max) // 2
+        print(self.x0, self.y0)
 
 
 def main():
-    w, h, n, x0, y0 = read_initial_config()
-    bounds_x = (0, w)
-    bounds_y = (0, h)
-
+    w, h, _, x0, y0 = read_initial_config()
+    bat = Batman(x0, y0, w, h)
     bomb_dir = "UR"
-    x, y = reposition(bomb_dir, x0, y0, bounds_x, bounds_y)
-    x, y = reposition("R", x, y, w, h)
-    x, y = reposition("L", x, y, w, h)
-    print(x, y)
+    bat.update_boundaries(bomb_dir)
+    bat.move()
+    bat.update_boundaries("DR")
+    bat.move()
+    bat.update_boundaries("DL")
+    bat.move()
 
 
 if __name__ == "__main__":
